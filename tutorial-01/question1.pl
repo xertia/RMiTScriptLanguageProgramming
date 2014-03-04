77d48614656292758141db69f289fa3832cc5856
Dane Leckey <daneau@techturf.com.au>
2014-03-05 00:42:47 +1100

    Implementation of strict and warning conformity in perl - omitted initially

--- tutorial-01/question1.pl
+++ tutorial-01/question1.pl
@@ -4,10 +4,12 @@
 ##  Question 1 :
 ##  Write a perl script to iterate through a list of numeric values. Print smallest and largest value. 
 ##  If less than two elements, show error message
+use strict;
+use warnings;
 
 ##  Define Array
-@elements = (-2,3,4,11,23,55);
-#@elements = (-3);
+my @elements = (-2,3,4,11,23,55);
+#my @elements = (-3);
 
 print "Tutorial 1 - Question 1\n";
 if(scalar(@elements) < 2) {
@@ -18,9 +20,9 @@ if(scalar(@elements) < 2) {
 
 ## Sort Array, get the first and last indexes to show 
 ## smallest and largest element values
-@sorted = sort(@elements);
-print("Smallest:  @sorted[0]\n");
-print("Largest: @sorted[scalar(@sorted)-1]\n");
+my @sorted = sort(@elements);
+print("Smallest:  $sorted[0]\n");
+print("Largest: $sorted[scalar(@sorted)-1]\n");
 
 ## Script Terminate
 exit;
--- tutorial-01/question2.pl
+++ tutorial-01/question2.pl
@@ -10,12 +10,15 @@
 ##  2). If date between 29-31, same as above but with warning 
 ##      regarding validity of day existing in given month
 ##  3). If given day > 31 || < 1 error printed.
+use strict;
+use warnings;
+
 
 # Number Randomly selected from set {1,2,3,4,5,6,7}
-$random_date = int(rand(6) + 1);
+my $random_date = int(rand(6) + 1);
 
 # Days of the Week Array
-@days = (
+my @days = (
 	"Monday",
 	"Tuesday",
 	"Wednesday",
@@ -30,7 +33,7 @@ print("Random Number: $random_date\n");
 
 # Get users date value, cleanse and use.
 print("Please enter your date (dd) >> ");
-$user_date = <>;
+my $user_date = <>;
 chomp($user_date);
 
 # Check validity of entered date.
@@ -44,13 +47,13 @@ print("\nYou Entered $user_date\n");
 ## First case, assume that for the most part that entered date
 ## likely is later than the generated date, check that date entered
 ## is later than that randomly generated.
-$index = 0;
+my $index = 0;
 
 if($user_date >= $random_date) {
 
-	for($i = $random_date; $i <= 31; $i++)	{		
+	for(my $i = $random_date; $i <= 31; $i++)	{		
 		if($i == $user_date)	{		
-			print("The day of $user_date is a @days[$index]\n");
+			print("The day of $user_date is a $days[$index]\n");
 			if($user_date > 29)	{
 				warn("WARNING: The Date May NOT Exist Within Current Month (Condition > 29)\n");
 			}
@@ -61,9 +64,9 @@ if($user_date >= $random_date) {
 	## Second case, the first monday of a given month is post the
 	## entered date value
 	
-	for($i = $random_date; $i > 0; $i--)	{
+	for(my $i = $random_date; $i > 0; $i--)	{
 		if($i == $user_date)	{
-			print("The day of $user_date is a @days[$index]\n");
+			print("The day of $user_date is a $days[$index]\n");
 		}
 		$index = $index-- > 0 ? $index-- : 6;
 	}
