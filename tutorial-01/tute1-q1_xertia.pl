10dfee7b04202bae9d68ce722389b089ac959d35
Luke Snelgar <xertia@gmail.com>
2014-03-06 20:32:43 +1030

    Here's my version
    Well, hope this works. I've forked and added my own file, don't really
    know what I'm doing yet :)

---
+++ tutorial-01/tute1-q1_xertia.pl
@@ -0,0 +1,25 @@
+# CPT223 - Perl Tutorial 1 - Question 1
+# Luke Snelgar s3347534@student.rmit.edu.au
+
+use strict;
+use warnings;
+
+#my @numbers = (1);   # invalid array size
+my @numbers = (-3, 2.5, 9, 0.76, 11);  # valid array size
+my $arraySize = @numbers;
+my $count = 0;
+
+if ($arraySize < 2)
+{
+	print "Error, not enough elements!\n";
+}
+else
+{
+	foreach my $num (sort @numbers)
+	{
+		print "Smallest number = $num\n" if ($count == 0);
+		print "Largest number = $num\n" if ($count == $arraySize-1);
+		$count++;
+	}
+}
+
